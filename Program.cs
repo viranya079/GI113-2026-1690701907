46429ad23862e5dafec0ab25940b00f27196f3ba
viranya079 <viranya.phae@bumail.net>
2026-09-04 16:50:03 +0700

    SUBMIT LAB03

--- Program.cs
+++ Program.cs
@@ -1,47 +1,58 @@
 ﻿/*
- * Student ID : 1690701907
- * Name       :viranya phaethong
- * Section    :129c
- * No.        :
- * Course     : GI113 Computer Programming (GI)
- */
+* Student ID : 1690701907
+* Name       :viranya phaethong
+* Section    :129c
+* No.        :
+* Course     : GI113 Computer Programming (GI)
+*/
 
-using System;
-
-namespace Lab02
+namespace LAB03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string bossName = "Kirin";
-            char rank = 'S';
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";  //ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';          //ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
             int level = 7;
             int maxHp = 240;
-            int currentHp = 175;
+            int currentHp = 115;    //คำสั่งต้นของ Lab นี้คือ HP "พลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
             float attackPower = 42.5f;
             double critMultiplier = 1.75;
             bool isBoss = true;
 
-            Console.WriteLine("===== BOSS STATUS: INITIAI ====='");
-            Console.WriteLine($"Name: {bossName}");
-            Console.WriteLine($"Rank; {rank}");
-            Console.WriteLine($"HP:{currentHp}/{maxHp}");
-            Console.WriteLine($"attack power: {attackPower}");
-            Console.WriteLine($"Crit Multiplier:{critMultiplier}");
-            Console.WriteLine($"Is boss: {isBoss}");
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName})" +
+                $"\nRank: {rank}" +
+                $"\nLevel: {level}/{MaxLevel}" +
+                $"\nHP: {currentHp}/{maxHp}" +
+                $"\nattack Power: {attackPower}" +
+                $"\nCrit Mutiplier: {critMultiplier}" +
+                $"$\nIs Boss: {isBoss}");
 
-            Console.WriteLine();
-            // คำนวนเปอร์เซ็น
-            int hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP Percent:{hpPercent}%");
-            Console.WriteLine();
-            Console.WriteLine($"Kirin takes 60 damage!");
-            Console.WriteLine();
-            // ลด HP 60 หน่วย
-            currentHp = currentHp - 60;
-            Console.WriteLine();
-            Console.WriteLine("===== BOSS STATUS: after damage =====");
-            Console.WriteLine($"HP: {currentHp}/{maxHp}");
-            hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP Percent:{hpPercent}%");
+            //1.Implicit Convaersion int (Hp) -->> double
+            Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+            double currentHpDouble = currentHp;
+            Console.WriteLine($"HP (doble): {currentHpDouble}");
+
+            //2.Calculate Percentage
+            Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+            double hpPercentExact = currentHpDouble = 100 / maxHp;
+            Console.WriteLine($"HP Percent (Exact): {hpPercentExact}");
+
+            //3.Explicit float (attactPower) -->> int
+            Console.WriteLine("\n----- Explicit float: attackPower as int -----");
+            double attackPowerintCast = (int)attackPower;
+            Console.WriteLine($"Attack Power (int cast): {attackPowerintCast}");
+
+            //4.Cast vs. Convert double (criMultiplier) -->> int
+            Console.WriteLine("\n-----Cast vs. Convert: criMultiplier -----");
+            int critMultiplierIntCast = (int)critMultiplier;
+            Console.WriteLine($"Critt Multiplier (int cast): {critMultiplierIntCast}");
+            int critiplierConvertRounded = Convert.ToInt32(critMultiplier);
+            Console.WriteLine($"Cast Multiplier (convert Rounded): {critiplierConvertRounded}");
+        }
+    }
+}
